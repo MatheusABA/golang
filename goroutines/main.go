58026c45c67f4e006020b9244aecf2f835b1a01e
MatheusABA <matheus.alencar02199@gmail.com>
2025-07-16 11:09:57 -0400

    [new]: Implementation of goroutines. -Created a function to learn goroutines basics -Direct called the function -Called the function using go routine -Called the variable that held the function with goroutine -Called the function with goroutine as anonymous function

---
+++ goroutines/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func fun(value string) {
+	for range 5 {
+		fmt.Println(value)
+		time.Sleep(1 * time.Millisecond)
+	}
+}
+
+func main() {
+	// Direct call to the function
+	fun("Direct call")
+
+	// Calling the function in a goroutine
+	go fun("First goroutine")
+
+	qr := fun
+
+	go qr("Second goroutine")
+
+	// Goroutine as an anonymous function
+	go func() {
+		fun("Third goroutine")
+	}()
+
+	// Ensuring the goroutine has time to run before the main function exits
+	time.Sleep(10 * time.Millisecond)
+
+	fmt.Println("Done...")
+}
